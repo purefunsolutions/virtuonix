5f00a98359b26074df61f9158cb01f135ba5a708
Mika Tammi <mikatammi@gmail.com>
2024-06-02 23:14:26 +0300

    HTTP Server Hello World
    Signed-off-by: Mika Tammi <mikatammi@gmail.com>

--- virtuonix/src/main.rs
+++ virtuonix/src/main.rs
@@ -1,3 +1,9 @@
-fn main() {
-    println!("Hello Virtuonix!");
+use warp::Filter;
+
+#[tokio::main]
+async fn main() {
+    // GET /hello/virtuonix => 200 OK with body "Hello, virtuonix!"
+    let hello = warp::path!("hello" / String).map(|name| format!("Hello, {}!", name));
+
+    warp::serve(hello).run(([127, 0, 0, 1], 3030)).await;
 }
